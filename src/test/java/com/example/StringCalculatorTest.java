a29261bf64619ded81102c29b105b0e6b43dede6
1204Vraj <vrajranipa7@gmail.com>
2025-07-04 23:44:41 +0530

    [Added Test 5.0] Added test for throwing exception(with negative numbers) when negative numbers occurs (failed).

--- src/test/java/com/example/StringCalculatorTest.java
+++ src/test/java/com/example/StringCalculatorTest.java
@@ -40,5 +40,25 @@ class StringCalculatorTest {
         assertEquals(3, calc.add("//;\n1;2"));
         assertEquals(10, calc.add("//!\n3!2!5"));
     }
+    @Test
+    void throwsExceptionOnNegativeNumbers() {
+        Exception ex = assertThrows(IllegalArgumentException.class, () -> {
+            StringCalculator.add("1,-2");
+        });
+        assertEquals("negative numbers not allowed -2", ex.getMessage());
+        Exception ex1 = assertThrows(IllegalArgumentException.class, () -> {
+            StringCalculator.add("1,-2,-3,-5,6");
+        });
+        assertEquals("negative numbers not allowed -2,-3,-5", ex1.getMessage());
+        Exception ex2 = assertThrows(IllegalArgumentException.class, () -> {
+            StringCalculator.add("//;\n1;2;-4");
+        });
+        assertEquals("negative numbers not allowed -4", ex2.getMessage());
+        Exception ex3 = assertThrows(IllegalArgumentException.class, () -> {
+            StringCalculator.add("//:\n1:-5:-3");
+        });
+        assertEquals("negative numbers not allowed -5,-3", ex3.getMessage());
+    }
+
 }
 
